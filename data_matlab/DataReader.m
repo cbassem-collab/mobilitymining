f5d8c99d9f087d4981d97859884a3d1b0a757fb3
huihanlhh <huihanli831@gmail.com>
2018-10-17 10:22:23 -0400

    Modified user_loc.csv

--- data_matlab/DataReader.m
+++ data_matlab/DataReader.m
@@ -2,7 +2,7 @@ load('realitymining.mat');
 
 fid = fopen( 'user_locs.csv', 'w' );
 
-fprintf(fid, 'User_id, date, areaID.cellID');
+fprintf(fid, 'User_id, date, areaID.cellID\n');
 for i = 2:106
     for m = 1: size(s(i).locs,1)
         a = sprintf('%.9f',s(i).locs(m,1));
