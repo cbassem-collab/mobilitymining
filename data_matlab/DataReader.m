0ede9a76fac076f472e22b4e97d09875de7e7388
huihanlhh <huihanli831@gmail.com>
2018-10-17 10:01:44 -0400

    user_locs extracted from matlab

---
+++ data_matlab/DataReader.m
@@ -0,0 +1,12 @@
+load('realitymining.mat');
+
+fid = fopen( 'user_locs.csv', 'w' );
+
+fprintf(fid, 'User_id, date, areaID.cellID');
+for i = 2:106
+    for m = 1: size(s(i).locs,1)
+        a = sprintf('%.9f',s(i).locs(m,1));
+        b = sprintf('%.5f',s(i).locs(m,2));
+        fprintf(fid, '%d,%s,%s\n',i-1, a, b);
+    end
+end
